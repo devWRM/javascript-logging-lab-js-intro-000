fa4846a385827435d763c3cd3aa0abc0c4fad92b
Wendy Raven McNair <wen.mcnair@gmail.com>
2019-12-08 15:02:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-console.console.error("Help Now!");
-console.error("Help Now!");
-console.log("Hold up there...");
-console.warn("Warning!");
-console.log("Hod up there...")
-console.console.log("Hold up there...");
-console.console.log("Hold up there...")
 console.log("Hold up there...");
